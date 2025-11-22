218a2bf85968c0922e3983e9f15948db21f9926a
ShaonK <shaonk640@gmail.com>
2025-11-23 01:32:19 +0600

    layout changes

--- app/layout.js
+++ app/layout.js
@@ -1,25 +1,20 @@
 "use client";
+
 import MobileStatusBar from "./components/MobileStatusBar";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-black">
       <body className="flex justify-center bg-black font-inter">
-
         {/* FULL PAGE CENTERED MOBILE VIEW */}
         <div className="w-[360px] min-h-screen bg-black text-white shadow-xl overflow-hidden relative">
-
           {/* Mobile Status Bar FIXED INSIDE */}
           <MobileStatusBar />
 
           {/* Page Content */}
-          <div className=" pt-9">
-            {children}
-          </div>
-
+          <div className="pt-9">{children}</div>
         </div>
-
       </body>
     </html>
   );
